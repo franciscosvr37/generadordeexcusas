43397463c13d39b6e6f5976ad1f78000ad76368f
franciscosvr37 <siglasconexion@gmail.com>
2025-08-17 20:46:28 +0000

    mi primer comit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird']; 
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car', 'the soup'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let excusa=""
+  let randomNumber = Math.floor(Math.random() * 4) + 1;
+  randomNumber=randomNumber-1
+
+function generarExcusa(nro){
+  excusa=""
+  excusa=who[nro]+' '+action[nro]+' '+what[nro]+' '+when[nro]
+  return excusa
+
+}
+
+let cadena=generarExcusa(randomNumber)
+document.getElementById("excuse").innerHTML = cadena;
   //write your code here
   console.log("Hello Rigo from the console!");
 };
